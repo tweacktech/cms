cdea9c6a2c33074b36cbbdcce56ebd6fe393b90f
tweacktech <daridatongs@gmail.com>
2023-03-01 17:18:08 +0100

    first upload

---
+++ database/migrations/2023_02_27_143050_create_earnings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEarningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('earnings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('blog_id')->constrained();
+            $table->decimal('total_dollars_earned', 8, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('earnings');
+    }
+}
